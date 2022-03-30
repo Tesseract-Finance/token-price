6384779021aba4d4545c75cbc94cb14a04667d96
Valentin Mihov <valentin.mihov@gmail.com>
2022-03-30 10:53:15 +0300

    Remove the 0.4.3 vaults

--- scripts/networks/matic.py
+++ scripts/networks/matic.py
@@ -39,11 +39,6 @@ def TOKEN_PRICES():
 
   crvUSDBTCETH = interface.CurveLPToken("0xdAD97F7713Ae9437fa9249920eC8507e5FbB23d3")
 
-  tVaultUSDC = interface.yEarnVault("0xEB02e1024cC16bCD28adE5A87D46257dc307E18C")
-  tVaultDAI = interface.yEarnVault("0xf5e364b9c07222cdec7d371c1422625593966c54")
-  tVaultBTC = interface.yEarnVault("0x7Cd28e21a89325EB5b2395591E86374522396E77")
-  tVaultETH = interface.yEarnVault("0xDF53c53E553524d13Fea7a4170856eb8b9C8a6EF")
-
   tVaultUSDC_V2 = interface.yEarnVault("0x57bDbb788d0F39aEAbe66774436c19196653C3F2")
   tVaultDAI_V2 = interface.yEarnVault("0x4c8C6379b7cd039C892ab179846CD30a1A52b125")
   tVaultBTC_V2 = interface.yEarnVault("0x6962785c731e812073948a1f5E181cf83274D7c6")
@@ -82,10 +77,6 @@ def TOKEN_PRICES():
       ("ETH_USDC", QUICK_ETH_USDC),
       ("ETH_DAI", QUICK_ETH_DAI),
       ("ETH_USDT", QUICK_ETH_USDT),
-      ("tvUSDC", tVaultUSDC),
-      ("tvDAI", tVaultDAI),
-      ("tvWBTC", tVaultBTC),
-      ("tvWETH", tVaultETH),
       ("crvUSDBTCETH", crvUSDBTCETH),
 
       ("tvUSDC", tVaultUSDC_V2),
